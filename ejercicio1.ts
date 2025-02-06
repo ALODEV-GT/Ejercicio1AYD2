5b3c0983c82e5139580510d9b45c91099e5161c8
Alex Alonzo <62099269+ALODEV-GT@users.noreply.github.com>
2025-02-06 17:27:50 -0600

    Add files via upload

---
+++ ejercicio1.ts
@@ -0,0 +1,20 @@
+
+function sortWord(word: string) {
+    const letters = word.split("")
+    let orderedWord = ""
+    const currentIndex: number = 1
+    const minLetter
+
+    for (let i: number = 0; i < letters.length; i++) {
+        for (let j = currentIndex; j < letters.length; j++) {
+            if(letters[i] < letters[j]){
+
+            }    
+        }
+    }
+}
+
+function areAnnagramm(word1: string, word2: string) {
+    sortWord("hola")
+}
+
